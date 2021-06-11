6d54c3f16fcfdc89ae0d8547c41f96724076f12f
DanteSeattle <gabrieldantelombardi@gmail.com>
2021-06-10 17:29:55 -0700

    assignment completed

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,24 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def note_contents=(contents)
+    contents.each do |c|
+      if c.strip != ""
+        self.notes.build(content: c)
+      end
+    end
+  end
+
 end
